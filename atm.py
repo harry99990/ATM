e2153c222047e9f4dbbae03b9aeb235826450400
harry99990 <xornjs9502@naver.com>
2025-03-12 18:04:23 +0900

    ✨ ATM
    입금 기능 추가.

--- atm.py
+++ atm.py
@@ -6,7 +6,13 @@ while True:
     num = input("원하는 기능을 입력해주세요.(1.입금, 2.출금, 3.영수증 보기, 4.종료")
 
     if num == "1":
-        print("입금 기능입니다.")
+        deposit_amount = input("입금할 금액을 입력해주세요. : ")
+        if deposit_amount.isdigit() and int(deposit_amount) > 0 :
+            balance += int(deposit_amount)
+            print(f'{deposit_amount}원 입금되었습니다. 총 금액은{balance}원 입니다.')
+        
+        else:
+            print("정신차리고, 제대로 숫자로 입금액을 눌러!!")
     if num == "2":
         print("출금 기능입니다.")
     if num == "3":
