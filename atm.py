db4270e5801f8ff588fcfb89f2f090c797c15416
harry99990 <xornjs9502@naver.com>
2025-03-13 18:42:16 +0900

    ✨ 영수증 보기 기능 추가(시계 기능까지^^)
    사용자 입력을 받아 영수증 내역을 볼수있도록 영수증메세지 출력기능까지 구현.

--- atm.py
+++ atm.py
@@ -1,5 +1,11 @@
 # balance : 통장에 들어있는 기본금액을 담는 변수
 # 1.입금, 2.출금, 3.영수증 보기, 4.종료
+
+#빈 리스트
+receipt = []
+#시간 넣어보기
+import datetime
+#원금
 balance = 3000
 
 while True:
@@ -10,6 +16,8 @@ while True:
         if deposit_amount.isdigit() and int(deposit_amount) > 0 :
             balance += int(deposit_amount)
             print(f'{deposit_amount}원 입금되었습니다. 총 금액은{balance}원 입니다.')
+            #영수증에 데이터 넣기
+            receipt.append(("입금", deposit_amount, balance))
         
         else:
             print("정신차리고, 제대로 숫자로 입금액을 눌러!!")
@@ -19,10 +27,21 @@ while True:
             withdraw_amount = min(balance, int(withdraw_amount))
             balance -= withdraw_amount
             print(f'고객님이 출금한 금액은{withdraw_amount}원 이고, 현재잔액은 {balance}원 입니다.')
+            #영수증에 데이터 넣기
+            receipt.append(("출금", withdraw_amount, balance))
         else:
             print("정신차리고, 제대로 숫자로 입금액을 눌러!!")
     if num == "3":
-        print("영수증 보기 입니다.")
+        if receipt:
+            print("==========영수증==========")
+            now = datetime.datetime.now()
+            print(now)
+            for i in receipt:
+                print(f'{i[0]} : {i[1]}원 | {i[2]}원')
+        else:
+            print("허거덩거덩스 내역이 없습니다...")
+                
+                
     if num == "4":
         print("종료 합니다.")
 
